abe774f1d5d338d2a2f648105c042e87c16c7513
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-12-17 14:07:56 +0300

    feat :  finish the db sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,28 +1,19 @@
-create database mini_dish_db;
-
-CREATE DATABASE product_management_db;
-
-\c mini_dish_db;
-
--- Création de l'utilisateur
-CREATE USER --creation db
-CREATE DATABASE product_management_db;
+CREATE DATABASE mini_dish_db;
 
 --connexion au db
-\c product_management_db;
+\c mini_dish_db;
 
 -- Création de l'utilisateur
 CREATE USER mini_dish_db_manager WITH PASSWORD '123456';
 
-
 --ajout privièges connection au db
-GRANT CONNECT ON DATABASE product_management_db TO product_manager_user;
+GRANT CONNECT ON DATABASE mini_dish_db TO mini_dish_db_manager;
 
 --ajout privilèges créations table
-GRANT CREATE ON DATABASE product_management_db TO product_manager_user;
+GRANT CREATE ON DATABASE mini_dish_db TO mini_dish_db_manager;
 
 -- Attribution des privilèges à l'utilisateur
-GRANT select,update,delete ON DATABASE mini_dish_db TO mini_dish_db_manager;
+GRANT select,update,insert,delete  ON  TO product_manager_user;
 
 
 
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,11 @@
 create type category_type as enum ('VEGETABLE','ANIMAL','MARINE','DAIRY','OTHER');
-create type dish_type as enum ();
+create type dish_type as enum ('START','MAIN','DESSERT');
+
+create table Dish (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    dish_type dish_type NOT NULL
+);
 
 create table  Ingredient (
     id SERIAL PRIMARY KEY,
@@ -7,10 +13,5 @@ create table  Ingredient (
     price numeric(10,2) NOT NULL,
     category category_type NOT NULL,
     id_dish INT,
-    constraint fk_dish foreign key id_dish references Dish(id);
-);
-create table Dish (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    type dish_type NOT NULL
+    constraint fk_dish foreign key (id_dish) references Dish(id)
 );
